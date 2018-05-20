fb9272c283a8c5d3eec038b3bdc27855f444108d
田原 一樹 <ssp0727.lnc@gmail.com>
2018-05-20 13:42:42 +0900

    fix file

--- server/startServer.js
+++ server/startServer.js
@@ -1,9 +1,16 @@
+import 'babel-polyfill';
 import Express from 'express';
+import { matchRoutes } from 'react-router-config';
+import pcRoute from 'client/pc';
+import spRoute from 'client/sp';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
 
 const { DEVICE } = process.env;
-const port = DEVICE === 'pc' ? 3001: 3002;
+const port = DEVICE === 'pc' ? 3001 : 3002;
+const route = DEVICE === 'pc' ? pcRoute : spRoute;
+
+
 const app = new Express();
 
 app.use(Express.static(`build/client/${DEVICE}`));
